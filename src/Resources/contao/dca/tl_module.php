254689b5b4d34c9a80f03b870eba83ab0704c665
reluem <lucas.rech@me.com>
2018-09-17 23:02:46 +0200

    remove moduleWizard from navbarModule DCA

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,50 +1,53 @@
 <?php
     
     
-    \ContaoCommunityAlliance\MetaPalettes\MetaPalettes::appendFields('tl_module', 'navigation', 'template', array('UIkit_navbarModules'));
+    // \ContaoCommunityAlliance\MetaPalettes\MetaPalettes::appendFields('tl_module', 'navigation', 'template',
+    //     array('UIkit_navbarModules'));
+    
+    
+    \Contao\CoreBundle\DataContainer\PaletteManipulator::create()
+        ->addField('UIkit_navbarModules', 'nav_legend',
+            \Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND)
+        ->applyToPalette('navigation', 'tl_module');
     
     Controller::loadDataContainer('tl_content');
     
-    $GLOBALS['TL_DCA']['tl_module']['fields']['UIkit_navbarModules'] = [
+    $GLOBALS['TL_DCA']['tl_module']['fields']['UIkit_navbarModules'] = array(
         'label' => &$GLOBALS['TL_LANG']['tl_module']['UIkit_navbarModules'],
         'exclude' => true,
         'inputType' => 'multiColumnWizard',
-        'eval' => [
+        'eval' => array(
             'tl_class' => 'clr ctb-mcw ctb-navbar-mcw',
             'columnFields' =>
-                [
-                    'module' =>
-                        [
-                            'label' => &$GLOBALS['TL_LANG']['tl_module']['module'],
-                            'exclude' => true,
-                            'inputType' => 'select',
-                            'options_callback' => ['tl_content', 'getModules'],
-                            'eval' => [
-                                'mandatory' => false,
-                                'style' => 'width: 300px',
-                                'chosen' => true,
-                                'submitOnChange' => true,
-                            ],
-                            'wizard' =>
-                                [
-                                    ['tl_content', 'editModule'],
-                                ],
-                        ],
+                array(
+                    'module' => array
+                    (
+                        'label' => &$GLOBALS['TL_LANG']['tl_content']['module'],
+                        'exclude' => true,
+                        'inputType' => 'select',
+                        'options_callback' => array('tl_content', 'getModules'),
+                        'eval' => array(
+                            'mandatory' => true,
+                            'chosen' => true,
+                            'submitOnChange' => true,
+                            'style' => 'width: 300px',
+                        ),
+                    ),
                     'floating' =>
-                        [
+                        array(
                             'label' => $GLOBALS['TL_LANG']['tl_module']['UIkit_navbarModules_floating'],
                             'inputType' => 'select',
-                            'options' => ['left', 'right'],
+                            'options' => array('left', 'right'),
                             'reference' => &$GLOBALS['TL_LANG']['MSC'],
-                            'eval' => ['style' => 'width: 150px', 'includeBlankOption' => true, 'chosen' => true],
-                        ],
+                            'eval' => array('style' => 'width: 150px', 'includeBlankOption' => true, 'chosen' => true),
+                        ),
                     
-                    'cssClass' => [
+                    'cssClass' => array(
                         'label' => $GLOBALS['TL_LANG']['tl_module']['UIkit_navbarModules_cssClass'],
                         'inputType' => 'text',
-                        'eval' => ['style' => 'width: 100px','rgxp' => 'txt'],
-                    ],
-                ],
-        ],
+                        'eval' => array('style' => 'width: 100px', 'rgxp' => 'txt'),
+                    ),
+                ),
+        ),
         'sql' => 'blob NULL',
-    ];
+    );
---
+++ src/Resources/contao/languages/de/tl_module.php
@@ -0,0 +1,20 @@
+<?php
+    /**
+     * Contao Open Source CMS
+     *
+     * Copyright (c) 2005-2018 Leo Feyer
+     *
+     * @author    reluem
+     * @license   GNU/LGPL
+     * @copyright reluem 2018
+     */
+    
+    /**
+     * Created by PhpStorm.
+     * User: lucas
+     * Date: 17.09.18
+     * Time: 22:55
+     */
+    
+    $GLOBALS['TL_LANG']['tl_module']['UIkit_navbarModules'][0] = "Navigationsmodule";
+    $GLOBALS['TL_LANG']['tl_module']['UIkit_navbarModules'][1] = "Zusätzliche Module in der Navigation ausgeben";
