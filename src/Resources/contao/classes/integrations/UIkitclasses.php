0bd914244b691c8ad8348aa9c5024b6b8d823956
reluem <lucas.rech@me.com>
2018-09-21 16:07:28 +0200

    change background addition to class string

--- src/Resources/contao/classes/integrations/UIkitclasses.php
+++ src/Resources/contao/classes/integrations/UIkitclasses.php
@@ -34,19 +34,19 @@
                     
                 } // add background to content elements
                 else {
-                    $objTemplate->class = "uk-tile uk-tile-" . $objTemplate->UIkit_background . ' uk-padding-small ' . $objTemplate->class;
+                    $objTemplate->class = $objTemplate->class . ' uk-tile uk-tile-' . $objTemplate->UIkit_background . ' uk-padding-small';
                 }
             };
             
         }
-        
+
 //        /**
 //         * manipulate the given form to add advanced css to the existing css class
 //         */
 //        public function extendFormCssClasses(Widget $objWidget, $strForm, $arrForm)
 //        {
 //            if (isset($objTemplate->UIkit_background) && $objTemplate->UIkit_background != '') {
-//                $objWidget->class .= "uk-tile uk-tile-" . $objWidget->UIkit_background . ' uk-tile-small ' . $objWidget->class;
+//                $objWidget->class .= "uk-tile uk-tile-" . $objWidget->UIkit_background . ' uk - tile - small ' . $objWidget->class;
 //                return $objWidget;
 //            }
 //        }
