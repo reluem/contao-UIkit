855d0ad5bd8827e4407c379692481e5177f6eee8
reluem <lucas.rech@me.com>
2019-03-04 14:57:58 +0100

    remove unneccesary SQL

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -151,8 +151,6 @@
                         'dcaPicker' => true,
                         'tl_class' => 'w50 wizard',
                     ],
-                    'sql' => "varchar(255) NOT NULL default ''",
-                
                 ],
                 'linkTitle' => [
                     'label' => &$GLOBALS['TL_LANG']['tl_content']['linkTitle'],
