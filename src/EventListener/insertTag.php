3b536bd82002bfd368766ccff3e6a4f2317bf637
reluem <lucas.rech@me.com>
2020-03-17 18:06:24 +0100

    update logging

--- src/EventListener/insertTag.php
+++ src/EventListener/insertTag.php
@@ -1,26 +1,28 @@
 <?php
-
-    declare(strict_types=1);
-
-/*
- * Contao UIkit Bundle
- * @copyright  Copyright (c) 2018-2020, reluem
- * @author     reluem
- * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
- * @link       http://github.com/reluem/contao-uikit
- */
-
-namespace Reluem\ContaoUIkitBundle\EventListener;
-
+    
+    declare(strict_types = 1);
+    
+    /*
+     * Contao UIkit Bundle
+     * @copyright  Copyright (c) 2018-2020, reluem
+     * @author     reluem
+     * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+     * @link       http://github.com/reluem/contao-uikit
+     */
+    
+    namespace Reluem\ContaoUIkitBundle\EventListener;
+    
     use Contao\Frontend;
-
+    use Psr\Log\LogLevel;
+    use Contao\CoreBundle\Monolog\ContaoContext;
+    
     class insertTag extends Frontend
     {
         public function iconInsertTag($strTag)
         {
             // Parameter abtrennen
             $arrSplit = explode('::', $strTag);
-
+            
             if ('icon' !== $arrSplit[0] && 'cache_icon' !== $arrSplit[0]) {
                 //nicht unser Insert-Tag
                 return false;
@@ -36,9 +38,14 @@ namespace Reluem\ContaoUIkitBundle\EventListener;
                 if (null !== $arrSplit[3]) {
                     $$classes = $arrSplit[3] . ' ';
                 }
-
+                
                 return '<span uk-icon="icon:' . $icon . '; ratio: ' . $ratio . '" class="' . $classes . '"></span>';
             }
-            \System::log('Invalid icon inserttag', __METHOD__, TL_ERROR);
+            \System::getContainer()
+                ->get('monolog.logger.contao')
+                ->log(LogLevel::WARNING, 'Invalid icon inserttag: ' . $strTag . 'on page ID', array(
+                    'contao' => new ContaoContext(__CLASS__ . '::' . __FUNCTION__, TL_GENERAL
+                    ),
+                ));
         }
     }
